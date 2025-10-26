f0f74187d9aeb3efeaf6f821bc1079f5744ee98f
Varien Janitra Nuralif Susanto <vrnjanitra@gmail.com>
2025-10-26 20:42:42 +0700

    socmed-ui: implemented posts card component

--- socmed-ui/src/app/pages/home/home.page.ts
+++ socmed-ui/src/app/pages/home/home.page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'vxsocmed-home',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
