0b3479dd7c490046917f906f09a4435d68038005
Varien Janitra Nuralif Susanto <vrnjanitra@gmail.com>
2025-10-26 20:26:31 +0700

    socmed-ui: added /home route and index page

---
+++ socmed-ui/src/app/pages/home/home.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vxsocmed-home',
+  imports: [],
+  templateUrl: './home.page.html',
+  styleUrl: './home.page.scss',
+})
+export class HomePage {
+
+}
