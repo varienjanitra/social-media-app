edf8a262c99b80b23cf59218d78d3f3f984d4124
Varien Janitra Nuralif Susanto <vrnjanitra@gmail.com>
2025-10-28 00:57:30 +0700

    socmed-ui: added user-management feature and user service

---
+++ socmed-ui/src/app/features/user-management/services/user-service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../data/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private httpService = inject(HttpClient);
+
+  baseUrl = 'http://localhost:5077';
+
+  getPosts$(): Observable<User[]> {
+    return this.httpService.get<User[]>(this.baseUrl + '/Users')
+  }
+
+}
