549da7889b4e6e10b80f44c7925163c623175802
Varien Janitra Nuralif Susanto <vrnjanitra@gmail.com>
2025-10-28 01:35:44 +0700

    socmed-ui: changed post-card text presentation template

--- socmed-ui/src/app/features/postings/components/post-card/post-card.ts
+++ socmed-ui/src/app/features/postings/components/post-card/post-card.ts
@@ -4,12 +4,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 import { EnrichedPost } from '../../data/post.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'vxsocmed-posts-card',
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    DatePipe
   ],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss',
